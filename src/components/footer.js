f74175acf0b990bdf04764fb9ca819f9579dfb74
Fajar Agung <fajarspace@192.168.1.7>
2022-06-20 18:50:24 +0700

    rebuilding site Mon Jun 20 18:50:24 WIB 2022

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+          <h1>Ini Footer</h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h2>Ini Adalah Components Header</h2>
+        </div>
+    )
+}
+
+export default Header
