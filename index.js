90d00a864a9eb1423065e4fa4b6fbefe61973638
fajaragngn <fajaragngn@gmail.com>
2022-06-23 10:28:50 +0700

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 /* ============ */
-const PORT = process.env.PORT || 4873;
+const PORT = process.env.PORT || 3000;
 
 /*---*/
 const app = express();
