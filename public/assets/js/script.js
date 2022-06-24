b9f3c121dcb084f8cd31d6c919b1826a4086061a
fajaragngn <fajaragngn@gmail.com>
2022-06-24 10:50:30 +0700

    rebuilding site Fri Jun 24 10:50:30 WIB 2022

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -29,7 +29,7 @@ $('#mangas').on('click', '.see-detail', function () {
     $('.modal-body').html(`
         <div class="text-center">
             <br>
-            <img src="https://data.whicdn.com/images/92353047/original.gif" class="rounded">
+            <img width="300px" src="https://i.pinimg.com/originals/d1/d6/c0/d1d6c0fe9c91839b97e361387b505b97.gif" class="rounded">
         </div>
     `);
 
